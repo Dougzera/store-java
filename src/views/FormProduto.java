065785b823e3ebe107a36185a26f7adb09bc2b7a
Dougzera <almir.acioly@gmail.com>
2018-11-07 00:38:39 -0300

    Listagem dos produtos no componente Table

--- src/views/FormProduto.java
+++ src/views/FormProduto.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -60,6 +61,8 @@ public class FormProduto extends javax.swing.JFrame {
     jScrollPane1 = new javax.swing.JScrollPane();
     jTableProdutos = new javax.swing.JTable();
     jButtonPesquisar = new javax.swing.JButton();
+    jLabelid = new javax.swing.JLabel();
+    jFormattedTextFieldid = new javax.swing.JFormattedTextField();
     jLabel1 = new javax.swing.JLabel();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -95,6 +98,11 @@ public class FormProduto extends javax.swing.JFrame {
     jButtonEditar.setText("Editar");
 
     jButtonExcluir.setText("Excluir");
+    jButtonExcluir.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jButtonExcluirActionPerformed(evt);
+      }
+    });
 
     jButtonCancelar.setText("Cancelar");
     jButtonCancelar.addActionListener(new java.awt.event.ActionListener() {
@@ -103,20 +111,23 @@ public class FormProduto extends javax.swing.JFrame {
       }
     });
 
-    jTableProdutos.setModel(new javax.swing.table.DefaultTableModel(
-      new Object [][] {
-        {null, null, null, null},
-        {null, null, null, null},
-        {null, null, null, null},
-        {null, null, null, null}
-      },
-      new String [] {
-        "Title 1", "Title 2", "Title 3", "Title 4"
+    jTableProdutos.setModel(tabelaProduto);
+    jTableProdutos.addMouseListener(new java.awt.event.MouseAdapter() {
+      public void mouseClicked(java.awt.event.MouseEvent evt) {
+        jTableProdutosMouseClicked(evt);
       }
-    ));
+    });
     jScrollPane1.setViewportView(jTableProdutos);
 
     jButtonPesquisar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/img/search.png"))); // NOI18N
+
+    jLabelid.setText("ID");
+
+    jFormattedTextFieldid.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jFormattedTextFieldidActionPerformed(evt);
+      }
+    });
 
     javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
     jPanel1.setLayout(jPanel1Layout);
@@ -124,24 +135,29 @@ public class FormProduto extends javax.swing.JFrame {
       jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(jPanel1Layout.createSequentialGroup()
         .addContainerGap()
-        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-          .addComponent(jButtonNovo, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabelDescProduto)
-            .addComponent(jLabel2)))
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addGroup(jPanel1Layout.createSequentialGroup()
-            .addComponent(jFormattedTextFieldDescProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jLabelid)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jFormattedTextFieldid, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
             .addComponent(jButtonPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
           .addGroup(jPanel1Layout.createSequentialGroup()
-            .addComponent(jFormattedTextFieldQntProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addGap(23, 23, 23)
-            .addComponent(jLabel3)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addComponent(jLabel2)
+              .addComponent(jLabelDescProduto))
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jFormattedTextFieldPrecoProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addComponent(jFormattedTextFieldDescProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jFormattedTextFieldQntProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addGap(3, 3, 3)
+                .addComponent(jFormattedTextFieldPrecoProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))))
           .addGroup(jPanel1Layout.createSequentialGroup()
+            .addComponent(jButtonNovo, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
             .addComponent(jButtonSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
             .addComponent(jButtonCancelar)
@@ -150,18 +166,22 @@ public class FormProduto extends javax.swing.JFrame {
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
             .addComponent(jButtonExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         .addContainerGap())
     );
     jPanel1Layout.setVerticalGroup(
       jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(jPanel1Layout.createSequentialGroup()
         .addContainerGap()
-        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-          .addComponent(jButtonPesquisar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addComponent(jButtonPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
           .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-            .addComponent(jLabelDescProduto)
-            .addComponent(jFormattedTextFieldDescProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addComponent(jLabelid)
+            .addComponent(jFormattedTextFieldid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabelDescProduto)
+          .addComponent(jFormattedTextFieldDescProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
           .addComponent(jFormattedTextFieldQntProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -169,17 +189,16 @@ public class FormProduto extends javax.swing.JFrame {
           .addComponent(jLabel3)
           .addComponent(jFormattedTextFieldPrecoProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         .addGap(18, 18, 18)
-        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
           .addComponent(jButtonNovo)
-          .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-            .addComponent(jButtonSalvar)
-            .addComponent(jButtonCancelar)
-            .addComponent(jButtonEditar)
-            .addComponent(jButtonExcluir)))
+          .addComponent(jButtonSalvar)
+          .addComponent(jButtonCancelar)
+          .addComponent(jButtonEditar)
+          .addComponent(jButtonExcluir))
         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
       .addGroup(jPanel1Layout.createSequentialGroup()
-        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(0, 251, Short.MAX_VALUE))
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(0, 104, Short.MAX_VALUE))
     );
 
     jLabel1.setText("Produtos");
@@ -225,19 +244,57 @@ public class FormProduto extends javax.swing.JFrame {
 
   private void jButtonSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSalvarActionPerformed
     // TODO add your handling code here:
-            insert();
-        
-        
-        try {
-            tabelaProduto.setResult(select());
-        } catch (SQLException ex) {
-            Logger.getLogger(FormProduto.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    insert();
 
-        jFormattedTextFieldDescProduto.setText("");
-        jFormattedTextFieldQntProduto.setText("");
-        jFormattedTextFieldPrecoProduto.setText("");
+    try {
+      tabelaProduto.setResult(select());
+    } catch (SQLException ex) {
+      Logger.getLogger(FormProduto.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    jFormattedTextFieldDescProduto.setText("");
+    jFormattedTextFieldQntProduto.setText("");
+    jFormattedTextFieldPrecoProduto.setText("");
   }//GEN-LAST:event_jButtonSalvarActionPerformed
+
+  private void jTableProdutosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableProdutosMouseClicked
+    // TODO add your handling code here:
+    jFormattedTextFieldid.setText(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 0).toString());
+    jFormattedTextFieldDescProduto.setText(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 1).toString());
+    jFormattedTextFieldQntProduto.setText(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 2).toString());
+    jFormattedTextFieldPrecoProduto.setText(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 3).toString());
+    
+    jButtonExcluir.setEnabled(true);
+  }//GEN-LAST:event_jTableProdutosMouseClicked
+
+  private void jFormattedTextFieldidActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFormattedTextFieldidActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_jFormattedTextFieldidActionPerformed
+
+  private void jButtonExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExcluirActionPerformed
+    // TODO add your handling code here:
+    int opcao = JOptionPane.showConfirmDialog(this,
+                "Deseja realmente excluir este registro?",
+                "Registro Excluido!",
+                JOptionPane.YES_NO_OPTION);
+        if (opcao == JOptionPane.YES_OPTION) {
+
+            delete(jFormattedTextFieldid.getText());
+
+            jFormattedTextFieldid.setText("");
+            jFormattedTextFieldDescProduto.setText("");
+            jFormattedTextFieldQntProduto.setText("");
+            jFormattedTextFieldPrecoProduto.setText("");
+            try {
+                tabelaProduto.setResult(select());
+            } catch (SQLException ex) {
+                Logger.getLogger(FormProduto.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            jButtonExcluir.setEnabled(false);
+
+        } 
+  }//GEN-LAST:event_jButtonExcluirActionPerformed
 
   /**
    * @param args the command line arguments
@@ -269,53 +326,53 @@ public class FormProduto extends javax.swing.JFrame {
     /* Create and display the form */
     java.awt.EventQueue.invokeLater(new Runnable() {
       public void run() {
-        try{
+        try {
           new FormProduto().setVisible(true);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
           Logger.getLogger(FormProduto.class.getName()).log(Level.SEVERE, null, ex);
         }
       }
     });
   }
-  
+
   public ResultSet select() {
-        ResultSet rs = null;
-        String SQL = "select * from produto";
-        try {
-            rs = stmt.executeQuery(SQL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return rs;
+    ResultSet rs = null;
+    String SQL = "select * from produto";
+    try {
+      rs = stmt.executeQuery(SQL);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-  
+    return rs;
+  }
+
   public void delete(String id) {
-        String SQL = "DELETE FROM produto WHERE id = " + id;
-        try {
-            this.stmt.executeUpdate(SQL);
-        } catch (SQLException ex) {
-           ex.printStackTrace();
-        }
+    String SQL = "DELETE FROM produto WHERE id = " + id;
+    try {
+      this.stmt.executeUpdate(SQL);
+    } catch (SQLException ex) {
+      ex.printStackTrace();
     }
-  
+  }
+
   public void insert() {
-        String SQL = "INSERT INTO produto(descricao,quantidade, preco_und, data_cad, data_atlz) " + "VALUES (?,?,?,now(),now())";
-        try {
-            PreparedStatement ps = this.conexao.getConexao().prepareStatement(SQL);
+    String SQL = "INSERT INTO produto(descricao,quantidade, preco_und, data_cad, data_atlz) " + "VALUES (?,?,?,now(),now())";
+    try {
+      PreparedStatement ps = this.conexao.getConexao().prepareStatement(SQL);
 
-            ps.setString(1, jFormattedTextFieldDescProduto.getText());
-            ps.setInt(2, Integer.parseInt(jFormattedTextFieldQntProduto.getText()));
-            
-            BigDecimal d = new BigDecimal( jFormattedTextFieldPrecoProduto.getText());
-            ps.setBigDecimal(3, d);
+      ps.setString(1, jFormattedTextFieldDescProduto.getText());
+      ps.setInt(2, Integer.parseInt(jFormattedTextFieldQntProduto.getText()));
 
-            ps.executeUpdate();
-            ps.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+      BigDecimal d = new BigDecimal(jFormattedTextFieldPrecoProduto.getText());
+      ps.setBigDecimal(3, d);
+
+      ps.executeUpdate();
+      ps.close();
+    } catch (SQLException ex) {
+      ex.printStackTrace();
     }
-  
+  }
+
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JButton jButtonCancelar;
@@ -327,10 +384,12 @@ public class FormProduto extends javax.swing.JFrame {
   private javax.swing.JFormattedTextField jFormattedTextFieldDescProduto;
   private javax.swing.JFormattedTextField jFormattedTextFieldPrecoProduto;
   private javax.swing.JFormattedTextField jFormattedTextFieldQntProduto;
+  private javax.swing.JFormattedTextField jFormattedTextFieldid;
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel jLabel3;
   private javax.swing.JLabel jLabelDescProduto;
+  private javax.swing.JLabel jLabelid;
   private javax.swing.JPanel jPanel1;
   private javax.swing.JScrollPane jScrollPane1;
   private javax.swing.JTable jTableProdutos;
